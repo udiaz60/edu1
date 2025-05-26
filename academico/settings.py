3138afc31cc6cb3ceba800ef310559b08ffebe80
Ulises <udiaz60@gmail.com>
2025-05-25 19:06:54 -0500

    host

--- academico/settings.py
+++ academico/settings.py
@@ -4,7 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+     'ec2-44-205-248-130.compute-1.amazonaws.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
